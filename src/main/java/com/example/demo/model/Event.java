a796080dad95e491ed4ec84ca8413c2c02bb6ed1
arebelo <anagmrebelo@hotmail.com>
2024-03-12 22:19:03 +0100

    Extend Event

--- src/main/java/com/example/demo/model/Event.java
+++ src/main/java/com/example/demo/model/Event.java
@@ -3,11 +3,13 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
 @Entity
 @Table(name = "events")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,6 +17,7 @@ public class Event {
     private int duration;
     private String location;
     private String title;
+    private LocalDate date;
     @ManyToMany
     @JoinTable (
             name = "event_guest",
@@ -22,6 +25,4 @@ public class Event {
             inverseJoinColumns = @JoinColumn(name="guest_id")
     )
     private List<Guest> guests;
-
-    //private List<Guest> speakers;
 }
